81bd5a8dd1cd0a44e28074520bf0a86a2fcede88
MirellaAP <mirellap070317@gmail.com>
2024-03-20 14:28:33 -0400

    Commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,22 @@
+const url = "https://back-end-ifms.vercel.app/";
 
-export default function Home() {
+export default async function Home() {
+
+  const resposta = await fetch(url,{
+    next: {
+      revalidate: 1
+    }
+  });
+  const campi = await resposta.json();
+ 
   return (
     <main>
-     
+      <h1>Home</h1>
+      {campi.map((campus) =>
+        <div>
+             <p>{campus.nome_campus}</p>
+        </div>
+      )}
     </main>
-  );
+  )
 }
